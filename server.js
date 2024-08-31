ab439a3984ccf6a89da5e470a43c07025681386e
Max100kaas <m.boesboxem@outlook.com>
2024-08-31 17:01:05 +0200

    ready-5

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const server = http.createServer(app)
 
 const db = mysql.createPool({
     connectionLimit: 10,
-    host: 'https://maxcodeandgames.nl:3306/',
+    host: 'https://maxcodeandgames.nl/',
+    port: 3306,
     user: 'Maxcodeandgames',
     password: process.env.DBPASSWORD,
     database: 'Maxcodeandgames',
